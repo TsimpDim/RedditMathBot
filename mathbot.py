6a70c8f30d58572c7197ecf529bf1dfc469ea8d7
Tsimp Dim <dimitristsimpitas@yahoo.gr>
2017-08-26 21:48:03 +0300

    Fix some Regex, broke some Regex

--- mathbot.py
+++ mathbot.py
@@ -13,7 +13,7 @@ def RedditFriendlyString(text):
     return text.replace('**','^').replace('*','\*')
 
 def Authenticate():
-    reddit = praw.Reddit('RedditMathBot',user_agent='Simple math bot v1.0')
+    reddit = praw.Reddit('RedditMathBot',user_agent='Simple math bot v1.4')
     print("Authentication as {} successfull".format(reddit.user.me))
     return reddit
 
@@ -32,59 +32,86 @@ def Execute(reddit):
                 print("\nFound a math problem")
                 com = comment.body.split("Solve: ",1)[1] #Seperate the arguements
         
-                print("Trying to process...")
-                exp = str.strip(regex.search( r'\[(.*?),' , com).group(1)) #The expression
-                variable = str.strip(regex.search( r',(.*?)\]', com).group(1)) #The variable to solve for
-                print("Expression to solve:",exp)
-                print("Variable to solve for:",variable)
+                print("Trying to process comment",com,"...")
 
+                    
+                try:
+                    exp = str.strip(regex.search( r'\[(.*?),' , com).group(1)) #The expression
+                    variable = str.strip(regex.search( r',(.*?)\]', com).group(1)) #The variable to solve for
+                    print("Expression to solve:",exp)
+                    print("Variable to solve for:",variable)
+
+                    x = Symbol(variable)
+                    
+                    result = solve(exp,x)
+                    print("Solution :",result)
+                    comment.reply("Solution : "+str(result))
+                except:
+                    print("Wrong syntax, aborting...")
+                    pass
                 
-                x = Symbol(variable)
-                
-                result = solve(exp,x)
-                print("Solution :",result)
-                comment.reply("Solution : "+str(result))
                 if(comment.id not in comments_replied_to): comments_replied_to.append(comment.id)
 
             if("!RedditMathBot|Diff:" in comment.body): #Differentiate an expression
                 print("\nFound a math problem")
                 com = comment.body.split("Diff: ",1)[1] #Seperate the arguements
                 
-                print("Trying to process...")
-                exp = str.strip(regex.search( r'\[(.*?),' , com).group(1)) #The expression
-                variable = str.strip(regex.search( r',(.*?),', com).group(0))[1] #The variable to solve for
-                times = str.strip(regex.search( r',[0-9]\]',com).group(0))[1] #How many times to diff
-                print("Differentiate exp :",exp)
-                print("For variable :",variable)
-                print("Times : ",times)
+                print("Trying to process comment",com,"...")
 
-                x = Symbol(variable)
-            
-                result = RedditFriendlyString(str(simplify(diff(exp,x,times))))
-                print("Solution :",result)
-                comment.reply("The derivative is : "+result)
+
+                try:
+                    exp = str.strip(regex.search( r'\[(.*?),' , com).group(1)) #The expression
+                    variable = str.strip(regex.search( r',(.*?),', com).group(0))[1] #The variable to solve for
+                    times = str.strip(regex.search( r',[0-9]\]',com).group(0))[1] #How many times to diff
+                    print("Differentiate exp :",exp)
+                    print("For variable :",variable)
+                    print("Times : ",times)
+
+                    x = Symbol(variable)
+                
+                    result = RedditFriendlyString(str(simplify(diff(exp,x,times))))
+                    print("Solution :",result)
+                    comment.reply("The derivative is : "+result)
+                except:
+                    print("Wrong syntax, aborting...")
+                    pass
+
                 if(comment.id not in comments_replied_to): comments_replied_to.append(comment.id)
 
             if("!RedditMathBot|Limit:" in comment.body): #Find the limit of an expression
                 print("\nFound a math problem")
                 com = comment.body.split("Limit: ",1)[1] #Seperate the arguements
                 
-                print("Trying to process...")
-                exp = str.strip(regex.search( r'\[(.*?),' , com).group(1)) #The expression
-                variable = str.strip(regex.search( r',(.*?),', com).group(0))[1] 
-                appr = str.strip(regex.search( r',(.*?)\]',com).group(0))[1] #What our variable is approaching
-                print("Limit of :",exp)
-                print("For variable :",variable)
-                print("Approaching : ",appr)
+                print("Trying to process comment",com,"...")
 
-                x = Symbol(variable)
-                
-                result = RedditFriendlyString(str(limit(exp,variable,appr)))
-                print("Solution :",result)
-                comment.reply("The limit is : "+result)
+
+                try:
+                    exp = str.strip(regex.search( r'(?<=\[)(.*?),' ,com).group(1)) #The expression
+                    variable = str.strip(regex.search( r',(.*),', com).group(1))
+                    appr = str.strip(regex.search( r',(.*?)\]',com).group(1))[2] #What our variable is approaching -- needs fixing
+                    print("Limit of :",exp)
+                    print("For variable :",variable)
+                    print("Approaching : ",appr)
+
+                    x = Symbol(variable)
+                    
+                    result = RedditFriendlyString(str(limit(exp,variable,appr)))
+                    print("Solution :",result)
+                    comment.reply("The limit is : "+result)
+                except:
+                    print("Wrong syntax, aborting...")
+                    pass
+
                 if(comment.id not in comments_replied_to): comments_replied_to.append(comment.id)
 
+            if("!RedditMathBot|Help" in comment.body):
+                help_msg = '''Hi! I\'m a simple MathBot for your run of the mill calculus problems.\n\n\nYou can summon me easily by including \'!RedditMathBot|<function>: <arguments>\' in your comment.\n\n\The available functions and their corresponding arguments are :\n\Solve: [<expression>,<variable>]\n\Diff: [<expression>,<variable>,<times_to_diff>] \n\Limit: [<expression>,<variable>,<approaching_value>]'''
+                #^^This is not readable, i know. Howver the markdown does not like to cooperate -- i'll look into fixing this
+                comment.reply(help_msg)
 
+                if(comment.id not in comments_replied_to): comments_replied_to.append(comment.id)
+
+                
             StoreIDs(comments_replied_to)
 
             
